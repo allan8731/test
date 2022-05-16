7dd6e1cd57a3975026d5f167f2d007c47b575b18
allan8731 <104746443+allan8731@users.noreply.github.com>
2022-05-16 22:36:58 +0900

    Update home.dart

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,16 +1,24 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repopractice/viewModel/homeVC.dart';
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class Home extends HookConsumerWidget {
   //todo:: switch ConsumerWidget to HookConsumerWidget
   const Home({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // useEffect(() {
+    //   // print(1);
+    //   //todo:: start api request hear
+    //   //1. make provider for data to save
+    //   //2. make method part for "users", "posts", "todos"
+    //   //3. during 1 to 2 use codebuilder
+    // },[]);
     return Scaffold(
       appBar: AppBar(actions: const [
         ButtonConsumer(str: 'users'),
@@ -21,20 +29,12 @@ class Home extends HookConsumerWidget {
           str: 'todos',
         ),
       ]),
-      body: Center(
-        child: ListView(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            ApiDataConsumer()
-            // ref.read(apiDataProvider.notifier).state.length>0? ApiDataConsumer():Text('no data'),
-          ],
-        ),
-      ),
+      body:ApiDataConsumer()
     );
   }
 }
 
-class ButtonConsumer extends ConsumerWidget {
+class ButtonConsumer extends HookConsumerWidget {
   final String str;
 
   const ButtonConsumer({Key? key, required this.str}) : super(key: key);
@@ -42,11 +42,12 @@ class ButtonConsumer extends ConsumerWidget {
   //todo first:: pass str props to widget without riverpod provider nor createState
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() => null,[]);
     //todo make button class for users,posts,...
     // throw UnimplementedError();
     return Consumer(builder: (context, ref, child) {
       return ElevatedButton(
-        //why use this button style? just pick it up among latest 3 types of button
+        //todo::why use this button style? just pick it up among latest 3 types of button
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.resolveWith<Color>(
             (Set<MaterialState> states) {
@@ -61,9 +62,10 @@ class ButtonConsumer extends ConsumerWidget {
           return ref
               //todo what's diff btw watch and read?
               .watch(apiDataProvider.notifier)
-              .generate(endpoint: "users", dio: Dio());
+              // .generate(endpoint: "users", dio: Dio());
+              .generate(endpoint: str);
         },
-        child: const Text("Users Data"),
+        child: str==null?Text('no Data'):Text("$str Data"),
       );
     });
   }
@@ -76,7 +78,24 @@ class ApiDataConsumer extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     ///todo how to describe data for clients using Widget, which one?? why??
     var show = ref.watch(apiDataProvider);
-    return Text(show.toString());
+    // print(show.runtimeType);
+    // return Text("${show.runtimeType}");
     // throw UnimplementedError();
+
+    if(show.runtimeType==List<dynamic>){
+      return Text("no data");
+    }else  return
+      ListView(
+      children: [ShowData(show).widgetShow()],
+    );
+  }
+}
+
+class ShowData {
+  var show;
+  ShowData(this.show);
+
+  widgetShow(){
+    return Text("${show.map((x){return x.id;})}");
   }
 }
