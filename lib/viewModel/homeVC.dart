7e36d75f36ee070ea771a40636662027f95caec6
allan8731 <104746443+allan8731@users.noreply.github.com>
2022-05-16 22:36:52 +0900

    Update homeVC.dart

--- lib/viewModel/homeVC.dart
+++ lib/viewModel/homeVC.dart
@@ -1,6 +1,7 @@
-
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repopractice/model/api_client.dart';
 
 //import model
 import 'package:repopractice/model/user.dart';
@@ -12,15 +13,29 @@ class ApiDataGenerator extends StateNotifier {
   //1. let it just be [] and control view using ture?A:B method
   //2. put initial value through generator
   //3. or......
-  ApiDataGenerator(String state) : super(state);
+  ApiDataGenerator(List state) : super(state);
 
-  void generate({required String endpoint, required dio}) async {
+  void generate({required String endpoint}) async {
     //todo serialize data and make model subscribes this one
-    final data = await dio.get(
-        "https://jsonplaceholder.typicode.com/$endpoint");
-    final responseBody = data.data;
-    state = [...responseBody];
+    final dio = Dio();
+    final client = Api_client(dio);
+    if (endpoint == "users") {
+      state = await client.getUsers();
+      // print(state);
+    } else if (endpoint == "todos") {
+      state = await client.getTodos();
+      // print(state);
+    } else {
+      state = await client.getPosts();
+      // print(state);
+    }
+
+    // final data = await dio.get(
+    //     "https://jsonplaceholder.typicode.com/$endpoint");
+    // final responseBody = data.data;
+    // state = [...responseBody];
   }
 }
 
-final apiDataProvider = StateNotifierProvider((ref) => ApiDataGenerator("users"));
+final apiDataProvider =
+    StateNotifierProvider((ref) => ApiDataGenerator(["inital"]));
